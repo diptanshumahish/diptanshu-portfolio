554e9facfba4ab7482e0f4cf7d585f3a5f712c89
diptanshumahish <diptanshu@fyipen.com>
2023-11-25 22:37:43 +0530

    optimized for mobile screens top section

--- src/components/portfolio/Header/Header.tsx
+++ src/components/portfolio/Header/Header.tsx
@@ -5,31 +5,33 @@ const hf = Herr_Von_Muellerhoff({ subsets: ["latin"], weight: ["400"] });
 
 export default function Header() {
     return (
-        <div className="pt-[10vh] ">
-            <div className=" text-center">
-                <span className={`${hf.className}`}>diptanshu Mahish</span>
-            </div>
-            <div className="lg:px-[5vw]  lg:py-[5vh] p-2 flex flex-col gap-4">
-                <div>
-                    <img
-                        src="/Assets/Header/r1.svg"
-                        alt="Imagery 1"
-                        className="h-auto w-full "
-                    />
+        <div className="pt-[10vh]  lg:h-fit h-screen flex items-center justify-center ">
+            <div>
+                <div className=" text-center">
+                    <span className={`${hf.className}`}>diptanshu Mahish</span>
                 </div>
-                <div>
-                    <img
-                        src="/Assets/Header/r2.svg"
-                        alt="Imagery 3"
-                        className="w-full h-auto"
-                    />
-                </div>
-                <div>
-                    <img
-                        src="/Assets/Header/r3.svg"
-                        alt="Imagery 2"
-                        className="w-full h-auto"
-                    />
+                <div className="lg:px-[5vw]  lg:py-[5vh] p-2 flex flex-col gap-4">
+                    <div>
+                        <img
+                            src="/Assets/Header/r1.svg"
+                            alt="Imagery 1"
+                            className="h-auto w-full "
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src="/Assets/Header/r2.svg"
+                            alt="Imagery 3"
+                            className="w-full h-auto"
+                        />
+                    </div>
+                    <div>
+                        <img
+                            src="/Assets/Header/r3.svg"
+                            alt="Imagery 2"
+                            className="w-full h-auto"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
